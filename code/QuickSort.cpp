14a58b61ced7ef2c2f97c693053a9d8a6b01697b
Cam Nguyen Tran <73975520+cnmeow@users.noreply.github.com>
2023-03-13 08:12:40 +0700

    Update QuickSort.cpp

--- code/QuickSort.cpp
+++ code/QuickSort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 using namespace std::chrono;
 const int n = 1000000;
-int a[1000005];
+int a[n+5], b[n+5];
 int partition(int l, int r) {
     int pivot = a[r];
     int i = (l - 1);
@@ -31,10 +31,15 @@ int main() {
     for (int Test = 1; Test <= 10; ++Test) {
         string filename = "input/test" + to_string(Test) + ".inp";
         ifstream fi(filename);
-        for (int i = 0; i < n; ++i) fi >> a[i];
+        for (int i = 0; i < n; ++i) {
+            fi >> a[i]; b[i] = a[i];
+        }
         auto starttime = high_resolution_clock::now();
         quickSort(0, n-1);
         auto finishtime = high_resolution_clock::now();
+        sort(b, b+n);
+        for (int i = 0; i < n; ++i)
+            if (a[i] != b[i]) return 0;
         cout << duration_cast<microseconds>(finishtime - starttime).count() <<'\n';
     }
     return 0;
